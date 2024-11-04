42e77874f29e5b698f1ea0820576c0b94630bd5b
Potion <1k0j2i4@gmail.com>
2024-11-04 13:24:19 +0900

    configure default

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
+// import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import sveltePre from 'svelte-preprocess';
+import { cssModules, linearPreprocess } from 'svelte-preprocess-cssmodules';
+
+const { typescript, scss } = sveltePre;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: linearPreprocess([typescript(), scss(), cssModules()]),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      // See below for an explanation of these options
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>'],
+      },
+    }),
+  },
 };
 
 export default config;
