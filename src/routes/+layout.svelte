d7fc9d9e4598f4176bdf9b1c2343bb2e7a607edc
DevPotion <156809181+P0t1on@users.noreply.github.com>
2024-12-11 14:55:43 +0000

    메뉴 구현

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,15 @@
   import Svgs from '$lib/Svgs.svelte';
   import type { LayoutData } from './$types';
   import './style.scss';
+  import { goto } from '$app/navigation';
 
+  type svgTypes = Parameters<typeof Svgs>[1]['type'];
+
+  const acts = {
+    shield: 'Defense',
+    home: 'Station',
+    meeting_room: 'Adventure',
+  } as const;
   // export let data: LayoutData;
   const { children }: { children: Snippet } = $props();
   onMount(() => {
@@ -11,8 +19,16 @@
   });
 </script>
 
-{#snippet activity(category: Parameters<typeof Svgs>[1]['type'])}
-  <Svgs onclick={() => console.log('H')} />
+{#snippet activity(category: svgTypes)}
+  <div
+    class="activity"
+    onclick={() =>
+      goto(`/activities/${acts[category as keyof typeof acts] ?? 'Station'}`)}
+    title="Defense"
+    role="presentation"
+  >
+    <Svgs type={category} />
+  </div>
 {/snippet}
 
 <svelte:head>
@@ -22,6 +38,32 @@
 <div id="rootLayout">
   <a href="/activities/Adventure">test</a>
 
-  {@render activity('shield')}
+  <div id="activities">
+    {@render activity('shield')}
+    {@render activity('meeting_room')}
+    {@render activity('home')}
+  </div>
   {@render children()}
 </div>
+
+<style>
+  div#activities {
+    position: absolute;
+    right: 16px;
+    top: 16px;
+
+    div.activity {
+      border-style: outset;
+      border-radius: 4px;
+      border-color: gray;
+      cursor: pointer;
+      padding: 4px;
+      margin: 4px;
+
+      &:active {
+        cursor: auto;
+        border-style: inset;
+      }
+    }
+  }
+</style>
