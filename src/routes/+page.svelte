b57bcbc630058e1ad4f1d8972efca21e41a9ef5b
DevPotion <156809181+P0t1on@users.noreply.github.com>
2024-12-13 13:08:23 +0000

    기본 페이지 생성

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import type { PageData } from './$types';
+  import { goto } from '$app/navigation';
+
+  let { data }: { data: PageData } = $props();
+
+  onMount(() => {
+    goto('/activities');
+  });
 </script>
 
-<div id="root"></div>
+<div id="main"></div>
 
 <style lang="scss">
 </style>
