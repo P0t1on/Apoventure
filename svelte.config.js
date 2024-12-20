c68fd9903b21d57413c830e65b91886bcd49f198
DevPotion <156809181+P0t1on@users.noreply.github.com>
2024-12-20 14:45:05 +0000

    path 개선

--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,10 @@ const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: linearPreprocess([typescript(), scss(), cssModules()]),
-
   kit: {
+    alias: {
+      $story: './src/storypacks',
+    },
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
