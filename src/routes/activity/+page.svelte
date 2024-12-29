13855e76306916b90433c7c74726786323a5175f
AmteurPotion <ini4343@naver.com>
2024-12-30 03:43:11 +0900

    h

--- src/routes/activity/+page.svelte
+++ src/routes/activity/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import box2d, { b2PolygonShape, b2World } from '@box2d/core';
   import type { PageData } from './$types';
+  import { browser } from '$app/environment';
 
   let { data }: { data: PageData } = $props();
+
+  if (!browser) {
+    // load page but server side
+  }
 
   onMount(() => {
     console.log(data);
